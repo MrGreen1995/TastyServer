7b421402ba2dd2c3ca5a24677acb131e9532a5b9
MrGreen1995 <mardonov2014@gmail.com>
2018-12-16 23:59:24 +0500

    Initial commit

---
+++ app/src/main/java/mealorder/odiljon/com/tastyserver/MainActivity.java
@@ -0,0 +1,22 @@
+package mealorder.odiljon.com.tastyserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signInBtn;
+    TextView sloganTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signInBtn = (Button)findViewById(R.id.main_sign_in_button);
+        sloganTxt = (TextView)findViewById(R.id.slogan_text_view);
+
+    }
+}
